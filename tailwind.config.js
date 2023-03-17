eaeb1defb68a07de4459f54077c0b42dd0cbbd76
Luis Fernando Mendoza <luisfernandomendoza@Luiss-MacBook-Pro.local>
2023-03-16 18:20:01 -0700

    [lafointaine-sounds 1] Tailwind configuration

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./src/pages/**/*.{html,js,ts,jsx,tsx}",
+    "./app/**/*.{js,ts,jsx,tsx}",
+    "./pages/**/*.{js,ts,jsx,tsx}",
+    "./components/**/*.{js,ts,jsx,tsx}",
+    "./src/**/*.{js,ts,jsx,tsx}"
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
